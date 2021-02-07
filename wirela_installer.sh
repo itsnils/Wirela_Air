9995ab458020a1e0b95ef4e329006e8f83170971
Twisy-ch <Twisy-ch@users.noreply.github.com>
2021-02-07 18:32:18 +0100

    new features

--- wirela_installer.sh
+++ wirela_installer.sh
@@ -1,14 +1,14 @@
 yes | sudo apt install python3-pip
-yes | sudo yes | apt-get install python-pigpio python3-pigpio
-yes | sudo yes | apt-get install libopenjp2-7
-yes | sudo yes | apt-get install python3-pymysql
-yes | sudo yes | pip3 install smbus
-yes | sudo yes | pip3 install scd30_i2c
-yes | sudo yes | pip3 install adafruit-blinka
-yes | sudo yes | pip3 install adafruit-circuitpython-dotstar
-yes | sudo yes | apt-get install libtiff5
-yes | sudo yes | pip3 install luma.oled
-yes | sudo yes | pip3 install getmac
+yes | sudo  apt-get install python-pigpio python3-pigpio
+yes | sudo  apt-get install libopenjp2-7
+yes | sudo  apt-get install python3-pymysql
+yes | sudo  pip3 install smbus
+yes | sudo  pip3 install scd30_i2c
+yes | sudo  pip3 install adafruit-blinka
+yes | sudo  pip3 install adafruit-circuitpython-dotstar
+yes | sudo  apt-get install libtiff5
+yes | sudo  pip3 install luma.oled
+yes | sudo  pip3 install getmac
 sudo pigpiod
 sudo rm -r /home/pi/Wirela_Air/Images/
 sudo mkdir /home/pi/Wirela_Air_Settings
